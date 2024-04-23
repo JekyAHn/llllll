21e8324adf1feb38bd57138cc9f1a8da76034849
JekyAHn <lemon1576@naver.com>
2024-04-23 23:53:47 +0900

    [jeky] 6주차과제

---
+++ Lecture06-HW/ConsoleApplication1/ConsoleApplication1/MSList.cpp
@@ -0,0 +1,45 @@
+#pragma once
+#include "MSList.h"
+template <typename T>
+void MSList<T>::add(T data) 
+{
+    MSNode<T>* new_node = new MSNode<T>(data);
+    if (!_head)
+    {
+        _head = new_node;
+    }
+    else
+    {
+        MSNode<T>* current = _head;
+        while (current->next_node)
+        {
+            current = current->next_node;
+        }
+        current->next_node = new_node;
+    }
+    _size++;
+}
+
+template <typename T>
+T& MSList<T>::operator[](int index) 
+{
+    MSNode<T>* current = _head;
+    for (int i = 0; i < index; ++i)
+    {
+        current = current->next_node;
+    }
+    return current->data;
+}
+template <typename T>
+
+void MSList<T>::clear()
+{
+    while (_head)
+    {
+        MSNode<T>* temp = _head;
+        _head = _head->next_node;
+        delete temp;
+    }
+    _size = 0;
+}
+
